7161295ce84d22dd8bde741b0619910abf5b010c
bharathjinka09 <bharathjinka09@gmail.com>
2020-08-10 14:45:41 +0530

    Updates

---
+++ precache-manifest.94d37699649a0220b74ea7adf48b9129.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "325be0cfc702adbbeb8e323fd4514070",
+    "url": "/react-bootstrap-tutorial/index.html"
+  },
+  {
+    "revision": "d17c8c932e7ecb9c0444",
+    "url": "/react-bootstrap-tutorial/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "77f1511312f239e29451",
+    "url": "/react-bootstrap-tutorial/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d17c8c932e7ecb9c0444",
+    "url": "/react-bootstrap-tutorial/static/js/2.e48c7bde.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-bootstrap-tutorial/static/js/2.e48c7bde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77f1511312f239e29451",
+    "url": "/react-bootstrap-tutorial/static/js/main.b298c15f.chunk.js"
+  },
+  {
+    "revision": "291922f7daa233ff0c26",
+    "url": "/react-bootstrap-tutorial/static/js/runtime-main.ce7fdf0d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-bootstrap-tutorial/static/media/logo.5d5d9eef.svg"
+  }
+]);
